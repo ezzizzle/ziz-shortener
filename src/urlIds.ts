d118d6a34863f5b2416316b9e3ebd22aac6b14b7
Eric <ezzizzle@users.noreply.github.com>
2020-08-26 21:37:37 +1000

    Added function to convert a urlId to a number

--- src/urlIds.ts
+++ src/urlIds.ts
@@ -1,7 +1,8 @@
 const idLength = 4;
 const urlIdMap = 'abcdefghijklmnopqrstuvwxyz'.split('');
+const urlIdRegex = new RegExp('^[a-z]{4}$', 'i');
 
-const generateUrlId = (numericId: number): string => {
+export const generateUrlId = (numericId: number): string => {
   if (numericId < 0 || numericId > (26 ** idLength)) {
     throw new Error('Number must be between 1 and 26^4 inclusive');
   }
@@ -17,4 +18,9 @@ const generateUrlId = (numericId: number): string => {
   return chars.join('').padStart(idLength, 'a');
 };
 
-export default generateUrlId;
+export const urlIdToNumber = (urlId: string): number => {
+  if (!urlIdRegex.test(urlId)) throw new Error(`urlID must match the format ${urlIdRegex.toString()}`);
+  const values = urlId.toLowerCase().split('').map((l) => urlIdMap.indexOf(l));
+
+  return (values[0] * (26 ** 3) + (values[1] * (26 ** 2)) + (values[2] * 26) + values[3]);
+};
